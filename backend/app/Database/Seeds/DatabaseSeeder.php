f02d3347cfdbc490a30b0a33b87fc87d4ee5bd59
ashklve <hilaryashleypagador@gmail.com>
2025-10-19 12:00:10 +0800

    refactor(databases): update database seeder to include users seeder

--- backend/app/Database/Seeds/DatabaseSeeder.php
+++ backend/app/Database/Seeds/DatabaseSeeder.php
@@ -8,8 +8,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call('App\\Database\\Seeds\\ClearDatabaseSeeder');
-
-        // $this->call('App\\Database\\Seeds\\<name of the seeder here>');
+        $this->call('App\\Database\\Seeds\\UsersSeeder');
     }
 }
